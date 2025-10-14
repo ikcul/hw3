12410c0b51f5b3e6d3389aa888b1b8055bd9cd6f
Daniel <simdaniel06@gmail.com>
2025-10-13 23:11:20 -0700

    heap.h

--- heap.h
+++ heap.h
@@ -1,5 +1,6 @@
 #ifndef HEAP_H
 #define HEAP_H
+#include <vector>
 #include <functional>
 #include <stdexcept>
 
@@ -61,14 +62,79 @@ public:
 
 private:
   /// Add whatever helper functions and data members you need below
-
-
-
+  std::vector<T> data;
+  void trickleUp(size_t loc);
+  void trickleDown(size_t loc);
+  size_t m_;
+  PComparator c_;
 
 };
 
 // Add implementation of member functions here
+template <typename T, typename PComparator>
+Heap<T, PComparator>::Heap(int m, PComparator c){
+  m_ = m;
+  c_ = c;
+}
+template <typename T, typename PComparator>
+Heap<T, PComparator>::~Heap(){
+  
+}
 
+
+template <typename T, typename PComparator>
+void Heap<T, PComparator>::trickleUp(size_t loc){
+  size_t curr = loc;
+  while (curr != 0){
+    size_t parent = (curr-1)/m_;
+    if (c_(data[curr], data[parent])){
+      std::swap(data[parent], data[curr]);
+      curr = parent;
+    }else{
+      return;
+    }
+  }
+
+}
+template <typename T, typename PComparator>
+void Heap<T, PComparator>::trickleDown(size_t loc){
+  size_t curr = loc;
+  while ((m_*curr) + 1 < data.size()){
+    size_t child = (m_*curr) + 1;
+    //finding the best child to swap with
+    for (size_t i = 2; i <= m_; i++){
+      size_t tempChild = (m_*curr) + i;
+      if (tempChild >= data.size()){
+        break;
+      }else{
+        if (c_(data[tempChild], data[child])){
+          child = tempChild;
+        }
+      }
+    }
+
+    if (c_(data[child], data[curr])){
+      std::swap(data[child], data[curr]);
+      curr = child;
+    }else{
+      return;
+    }
+  }
+
+}
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::push(const T& item){
+  data.push_back(item);
+  trickleUp(data.size() - 1);
+}
+template <typename T, typename PComparator>
+bool Heap<T,PComparator>::empty() const{
+  return data.empty();
+}
+template <typename T, typename PComparator>
+size_t Heap<T, PComparator>::size() const{
+  return data.size();
+}
 
 // We will start top() for you to handle the case of 
 // calling top on an empty heap
@@ -81,12 +147,12 @@ T const & Heap<T,PComparator>::top() const
     // ================================
     // throw the appropriate exception
     // ================================
-
+    throw std::out_of_range("heap is empty");
 
   }
   // If we get here we know the heap has at least 1 item
   // Add code to return the top element
-
+  return data[0];
 
 
 }
@@ -101,13 +167,18 @@ void Heap<T,PComparator>::pop()
     // ================================
     // throw the appropriate exception
     // ================================
+    throw std::out_of_range("heap is empty");
 
-
+  }
+  std::swap(data[0], data[data.size() - 1]);
+  data.pop_back();
+  if (!empty()){
+    trickleDown(0);
   }
 
 
-
 }
+
 
 
 
